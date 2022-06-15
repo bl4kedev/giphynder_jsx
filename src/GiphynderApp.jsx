b0f737621dddcd8c135fb2bf8e0d61c9c96ee40c
Bl4ke <bl4akedev@outlook.com>
2022-06-14 21:29:10 -0600

    Estructura inicial del proyecto

---
+++ src/GiphynderApp.jsx
@@ -0,0 +1,33 @@
+export const GiphynderApp = () => {
+  return (
+    <div className="
+        bg-slate-900
+        w-full
+        h-screen
+        text-white
+        text-center
+    ">
+        
+        <div className="
+            container
+            w-10/12
+            mx-auto
+        ">
+
+            <h1 className="
+                text-4xl
+                font-bold
+                py-9
+            ">
+                Giphynder.jsx
+            </h1>
+
+            {/* Input */}
+
+            {/* List grid gifs */}
+
+        </div>
+
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GiphynderApp } from './GiphynderApp';
+
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GiphynderApp />
+  </React.StrictMode>
+)
